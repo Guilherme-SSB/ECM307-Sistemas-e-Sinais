27f61748b76c575850182ffb33e3106a7c00bdcd
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-06-02 09:52:56 -0300

    Criação da função que calcula os melhores valores de b e c

---
+++ Trabalhos/T3/erro_minimo.m
@@ -0,0 +1,22 @@
+function [b_bckup, c_bckup, erro_min] = erro_minimo(b, c, Vp, t1, t2)
+  erro_min  = 9999999;
+  N         = 1;
+  Trecho_escolhido = Vp(t1:t2);   % intervalo de 28ms a 36ms deltaT = 8ms
+  tic;
+  for i = 1: length(b)              %%%%%%%%%
+    for j = 1: length(c)            %% Avalia cada dupla (b, c)
+      D       = [1 b(i) c(j)];            % denominador
+      Gs      = tf(N, D);           % funcao de transferencia estimada
+      [serie, ~, ~] = impulse(Gs);
+      erro = sum(power(Trecho_escolhido - serie(1:(t2-t1+1)), 2));  % calcula o erro para (b, c) atual
+      
+      if erro < erro_min            % atualiza o erro minimo encontrado e (b, c)
+        erro_min = erro;
+        b_bckup = b(i);
+        c_bckup = c(j);
+      end        
+    end
+  end
+  fprintf("Tempo para achar o erro minimo: ");
+  toc;
+end
